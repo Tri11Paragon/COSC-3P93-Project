3d5dad65e3ab03a477f1fc9ff3e924e4a677efb9
Brett <brettmaster1@gmail.com>
2022-10-23 23:58:15 -0400

    fix for cmake

--- commit.sh
+++ commit.sh
@@ -2,4 +2,5 @@
 git add *
 git commit
 git push -u origin main
-wget "https://ci.tpgc.me/job/3P93%20Raytracing%20Project/build?token=sexualfavoursfromsexymen"
+curl "https://ci.tpgc.me/job/3P93%20Raytracing%20Project/build?token=sexualfavoursfromsexymen"
+curl "https://ci.tpgc.me/job/3P93%20Project/build?token=sexualfavoursfromsexymen"
---
+++ runCurrent.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+cd Step\ 3/build
+./Step_3
